efb64c4804258d9cc2bedf5b79d7d1e109d26f67
Ankit Anand <ankit854100@gmail.com>
2021-07-08 14:29:05 +0530

    initial commit of math.py file

---
+++ math.py
@@ -0,0 +1,8 @@
+#add functiondef add(x,y):
+    return x + y;#subtract function
+def subtract(x,y):
+    return x - y #on master #multiply function
+def multiply(x,y):
+    pass#divide function
+def divide(x,y):
+    pass	def square(x):	return x*x #on Bug123
